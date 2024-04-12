53acffe9d37225410108341db34fb24678e932fe
robertrose85 <robertrose85@gmail.com>
2024-04-11 20:41:15 -0400

    Included Jupyter, Heatmap, and working web app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 import pymongo
-import datetime
 import folium
 from flask import Flask, render_template, url_for, request, redirect
 
@@ -14,10 +13,6 @@ app = Flask(__name__)
 #DB Connetion String
 client = pymongo.MongoClient(connection_string)
 collection = client['GEOJSON']['GEOJSONTest']
-#collection = client['test']['worldairquality']
-
-l = list(collection.find({}))
-
 
 @app.route("/")
 def index():
@@ -25,97 +20,43 @@ def index():
 
 @app.route("/", methods=['GET', 'POST'])
 def search():
+    # Create basic Folium Map for page load
     m = folium.Map()
     if request.method == "POST":        
-        # Create      
 
-        search_field = request.form.get("search_field")
+        # Grab field values     
+        min_mag = request.form.get("min_mag")
+        max_mag = request.form.get("max_mag")
+
         try:
-            magnitude = collection.find_one({"properties.mag": {"$gte": float(search_field)}}) # {'properties.mag': {$gte: 4.5, $lt: 4.7}}
+            for eq in collection.find({"properties.mag": {"$gte": float(min_mag), "$lte": float(max_mag)}}):
 
-            ## Error handling for null results
-            longitude = magnitude['geometry']['coordinates'][0] 
-            latitude = magnitude['geometry']['coordinates'][1]
+                longitude = eq['geometry']['coordinates'][0] 
+                latitude = eq['geometry']['coordinates'][1]
 
-            print("Before IF statement")
+                if eq and latitude and longitude:
+                    folium.CircleMarker(
+                        location=[latitude, longitude],
+                        radius=10,
+                    ).add_to(m)
 
-            if magnitude and latitude and longitude:
-
-                folium.CircleMarker(
-                    location=[latitude, longitude],
-                    radius=10,
-                ).add_to(m)
-                m.save("static/folium.html")
-                # return redirect("/folium")
-            
+            m.save("static/folium.html")
 
             return render_template(
                 "index.html",
                 title="Results Found",
-                magnitude=magnitude,
+                magnitude=eq,
                 latitude=latitude,
                 longitude=longitude,
-                # iframe=iframe,
+                max_mag=max_mag,
             )
+
         except Exception as e:
             return e
-
-            # return render_template('index.html', title = "Results Found", magnitude = magnitude, latitude = latitude, longitude = longitude)
-        # except Exception as e:
-        #     return e    
-
-# @app.route("/", methods=['GET', 'POST'])
-# def search():
-#     if request.method == "POST":
-#         m = folium.Map(zoom_start=1)
-#         # print("in post handler")
-#         search_field = request.form.get("search_field")
-#         # print(f"search field: {search_field}")
-#         try:
-#             country_search = collection.find_one({"Country Code": search_field})
-#             # print(f"country_search: {country_search}")
-
-#             latitude = country_search["Latitude"]
-#             longitude = country_search["Longitude"]
-#             # print(f"lat and long: {latitude}, {longitude}")
-
-#             # Latitude': 35.908333, 'Longitude': 140.052778
-#             if country_search and latitude and longitude:
-
-#                 folium.CircleMarker(
-#                     location=[latitude, longitude],
-#                     radius=10,
-#                 ).add_to(m)
-#                 m.save("static/folium.html")
-#                 # return redirect("/folium")
-
-#             return render_template(
-#                 "index.html",
-#                 title="Results Found",
-#                 country_search=country_search,
-#                 latitude=latitude,
-#                 longitude=longitude,
-#                 # iframe=iframe,
-#             )
-#         except Exception as e:
-#             return e
-
 
 @app.route("/folium")
 def folium_endpoint():
     return render_template("folium.html", title="Folium Page")
         
-@app.route("/results")
-def results():
-    return render_template('results.html', title = "Layout Page")
-
-
-### Delete?
-@app.route("/map")
-def geo():
-    m = folium.Map([43, -100], zoom_start=4)
-    folium.GeoJson(l).add_to(m)
-    return m.get_root().render()
-
 if __name__ == "__main__":
     app.run(debug=True)
