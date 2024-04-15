d7eadc86645bc5bede5a0a06461e4353b2d79fa3
robertrose85 <robertrose85@gmail.com>
2024-04-15 17:32:56 -0400

    Edited map and added popups

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import pymongo
 import folium
 from flask import Flask, render_template, url_for, request, redirect
+import branca
 
 # Grab Connection String - Ensure this file are part of your .gitignore
 connection_string = open("connection_string.txt")
@@ -31,13 +32,40 @@ def search():
         try:
             for eq in collection.find({"properties.mag": {"$gte": float(min_mag), "$lte": float(max_mag)}}):
 
+                title = eq['properties']['title']
                 longitude = eq['geometry']['coordinates'][0] 
                 latitude = eq['geometry']['coordinates'][1]
-
+                magnitude = eq['properties']['mag']
+                link = eq['properties']['url']
+                
+                            
                 if eq and latitude and longitude:
-                    folium.CircleMarker(
+
+                    popup_html = """
+                        <!DOCTYPE html>
+                        <style>
+                        
+                        </style>
+                        <base target="_blank">
+                        <h4>{}</h4>
+                        <p>
+                        <b>Latitude:</b> {}
+                        <br>
+                        <b>Longitude:</b> {}
+                        <br>
+                        <b>Magnitude:</b> {}
+                        <br>
+                        <hr>
+                        For more Info: <br> <a href="{}">Visit USGS</a>
+                        </p>
+                            """.format(title, latitude, longitude, magnitude, link)
+                    iframe = branca.element.IFrame(html=popup_html, width=350, height=250)
+                    popup = folium.Popup(iframe, max_width=500)
+
+                    folium.Marker(
                         location=[latitude, longitude],
                         radius=10,
+                        popup=popup
                     ).add_to(m)
 
             m.save("static/folium.html")
