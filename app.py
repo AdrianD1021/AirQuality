239556962360e83b04c78db37eae47bf480c933f
robertrose85 <robertrose85@gmail.com>
2024-04-19 09:50:06 -0400

    Updated dynamic bargraph

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import folium.plugins
 from pymongo import MongoClient
 import pymongo
 import folium
-from flask import Flask, render_template, url_for, request, redirect
-from folium.plugins import HeatMap
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 import branca
+import math
 
 # Grab Connection String - Ensure this file are part of your .gitignore
 connection_string = open("connection_string.txt")
@@ -12,6 +12,7 @@ connection_string = connection_string.read()
 
 # Assign Flask app
 app = Flask(__name__)
+
 
 #DB Connetion String
 client = pymongo.MongoClient(connection_string)
@@ -31,8 +32,9 @@ def search():
         # Grab field values     
         min_mag = request.form.get("min_mag")
         max_mag = request.form.get("max_mag")
-        print(min_mag)
+        # print(min_mag)
 
+        # Assign valid default values from the DB - This can be automated in the future.
         if not min_mag or not max_mag:
             min_mag = 2.0
             max_mag = 7.4
@@ -47,9 +49,11 @@ def search():
                 magnitude = eq['properties']['mag']
                 link = eq['properties']['url']
 
-                if magnitude <= 4.5:
+                if magnitude <= 4.0:
                     color = 'blue'
-                elif magnitude > 4.5 and magnitude <= 6.5:
+                elif magnitude <= 5.0:
+                    color = 'green'
+                elif magnitude <= 6.5:
                     color = 'orange'
                 else:
                     color = 'red'
@@ -105,13 +109,60 @@ def folium_endpoint():
     return render_template("folium.html", title="Folium Page")
 
 
-@app.route("/graph")
-def graph():
+@app.route("/test")
+def test():
     return render_template('graph.html', title = "Graph Page")
 
 @app.route("/heatmap")
 def heatmap():
     return render_template('map.html', title = "Map Page")
+
+@app.route("/graph")
+def graph_index():
+    return render_template('graph.html')
+
+@app.route("/graph", methods=['GET', 'POST'])
+def graph():
+    data = {}
+
+    if request.method == "POST":  
+    
+        # Grab field values     
+        min_mag = request.form.get("min_mag")
+        max_mag = request.form.get("max_mag")
+        # print(min_mag)
+
+
+    # Assign valid default values from the DB - This can be automated in the future.
+        if not min_mag or not max_mag:
+            min_mag = 2.0
+            max_mag = 7.4
+
+    # Look through results
+        try:
+            data_collection = collection.find({"properties.mag": {"$gte": float(min_mag), "$lte": float(max_mag)}})
+
+            for d in data_collection:
+                value = d['properties']['mag']
+                label = int(math.floor(value))
+
+                # Increment label values
+                if label not in data:
+                    data[label] = 1
+                else:
+                    data[label] +=1
+            values = sorted(list(data.items()))
+
+            # For Chart.js, values need to be put into list.
+            labels = [row[0] for row in values]
+            data = [row[1] for row in values]
+
+            print(labels)
+            # print(values[0][0]) # returns 4
+                
+            return render_template('graph.html', min_mag=min_mag, max_mag=max_mag, labels=labels, data=data)
         
+        except Exception as e:
+            return e
 if __name__ == "__main__":
     app.run(debug=True)
